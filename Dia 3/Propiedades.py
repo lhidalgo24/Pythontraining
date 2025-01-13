3c1e61ea129dce9b0c1569052dbdbff29e4cd41f
lhidalgo24 <lovelp_hg@hotmail.com>
2025-01-12 23:07:27 -0500

    Dia 3

---
+++ Dia 3/Propiedades.py
@@ -0,0 +1,19 @@
+nombre = "Carina"
+nombre = "Karina"
+print(nombre)
+
+n1 = "Kari"
+n2 = "na"
+print(n1+n2)
+
+n1 = "Kari"
+n2 = "na"
+print(n1*10)
+
+poema = """Mil pequeños peces blancos
+comosi hirviera
+el color del agua"""
+print(poema)
+print("agua" in poema)
+print("sol" not in poema)
+print(len(poema))
