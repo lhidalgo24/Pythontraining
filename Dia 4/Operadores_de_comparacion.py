55f7ff7661956d88766cdbf6e82f0af4dc4fcf6c
Lovel Hidalgo <lovel.hidalgo@tranzact.net>
2025-01-14 18:27:16 -0500

    Dia 4

---
+++ Dia 4/Operadores_de_comparacion.py
@@ -0,0 +1,8 @@
+mi_variable = 'Hola mundo'
+mi_bool = 5+5==18-8
+mi_bool = 'blanco'=='Blanco'.lower()
+mi_bool = 100.0 == 100
+mi_bool = 100!=99
+mi_bool = 100>99
+print(mi_bool)
+
---
+++ Dia 4/Operadores_logicos.py
@@ -0,0 +1,11 @@
+mi_bool = (4 < 5) and (5 == 2+3)
+mi_bool = (55 == 55) and ('perro' == 'perro')
+mi_bool = (1==10) or (3==3)
+print(mi_bool)
+
+texto  = "Esta Frase es Breve"
+mi_bool = ("Frase" in texto) and ("Breve" in texto)
+print(mi_bool)
+
+mi_bool = not 'a' == 'a'
+print(mi_bool)
