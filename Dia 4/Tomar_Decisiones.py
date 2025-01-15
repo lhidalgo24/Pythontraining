263d490da77461e9f7d3a6b890b9e04851e2cce1
lhidalgo24 <lovelp_hg@hotmail.com>
2025-01-14 21:33:11 -0500

    Dia 3

--- Dia 4/Tomar_Decisiones.py
+++ Dia 4/Tomar_Decisiones.py
@@ -1,2 +1,34 @@
 if 10 > 9:
-    
+    print("Es correcto")
+
+x = True
+if x:
+    print("Es correcto")
+
+if 5==2:
+    print("Es correcto")
+else:
+    print("No es correcto")
+
+mascota = "perro"
+if  mascota == "gato":
+    print("Tienes un gato")
+elif mascota == "perro":
+    print("Tienes un perro")
+elif mascota == "pez":
+    print("Tienes un pez")
+else:
+    print("No se qué animal tienes")
+
+
+edad = 16
+calificacion = 9
+
+if edad<18:
+    print("Eres menor de edad")
+    if calificacion >= 7:
+        print("Aprobado")
+    else:
+        print("Desaprobado")
+else:
+    print("Eres adulto")
